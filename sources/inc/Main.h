7ae68ad80615cf8a91982fcfdb007e3a32a9b5c2
Ofghanirre <aleronraider@gmail.com>
2023-11-09 15:19:36 +0100

    feat(gitignore): update gitignore

---
+++ sources/inc/Main.h
@@ -0,0 +1,14 @@
+//
+// Created by Antonin Jean on 09/11/2023.
+//
+
+#ifndef PROCEDURALGEN_MAIN_H
+#define PROCEDURALGEN_MAIN_H
+
+
+class Main{
+
+};
+
+
+#endif //PROCEDURALGEN_MAIN_H
--- sources/src/Main.cpp
+++ sources/src/Main.cpp
@@ -1,19 +1,5 @@
 //
-// Created by Antonin on 29/10/2023.
+// Created by Antonin Jean on 09/11/2023.
 //
-#include <iostream>
-#include <glm/common.hpp>
-int main(int argc, char *argv[])
-{
-    std::cout << "test";
-    std::cout << "argc == " << argc << '\n';
 
-    for (int ndx{}; ndx != argc; ++ndx)
-        std::cout << "argv[" << ndx << "] == " << argv[ndx] << '\n';
-    std::cout << "argv[" << argc << "] == "
-              << static_cast<void*>(argv[argc]) << '\n';
-
-    /*...*/
-
-    return argc == 3 ? EXIT_SUCCESS : EXIT_FAILURE; // optional return value
-}
+#include "Main.h"
