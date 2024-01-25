80040c86b252cbe727f5b356ddc65f07fd6c4896
Ofghanirre <aleronraider@gmail.com>
2024-01-25 16:31:32 +0100

    update main

--- sources/src/Main.cpp
+++ sources/src/Main.cpp
@@ -2,6 +2,12 @@
 // Created by Antonin Jean on 25/01/2024.
 //
 
+#include "inc/api/noise/BitMap.h"
+#include <iostream>
+
 int main(void) {
+    BitMap<int> bitmap(16,16, 0);
+    bitmap[0] = 1;
+    std::cout << bitmap[0] << std::endl;
     return 0;
 }
