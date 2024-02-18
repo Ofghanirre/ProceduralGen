3e21624e6d6ae3f8dd0d67fbae32241d7d732289
Ofghanirre <aleronraider@gmail.com>
2024-02-18 20:00:12 +0100

    feat(project): refresh main & implements punderation

--- sources/src/Main.cpp
+++ sources/src/Main.cpp
@@ -8,45 +8,51 @@
 #include <api/Terrain.h>
 #include <Perlin.hpp>
 #include <Worley_noise.hpp>
+#include <time.h>
 
-
-int main(void) {
-    Terrain terrain("test", 0);
+void testPerlin() {
+    Terrain terrain1("test", 0);
     Perlin p = Perlin(0, 255, 30);
-    terrain.addTerrainNode(p, NoisePunderationFunction::IDENTITY);
-    Noise result = terrain.getTerrain(256, 256, 42);
-    result.toPGM("./perlin.pgm");
+    terrain1.addTerrainNode(p, NoisePunderationFunction::IDENTITY);
+    Noise result1 = terrain1.getTerrain(256, 256, 42);
+    result1.toPGM("./perlin.pgm");
+}
 
+void testWorley() {
     Terrain terrain2("test2", 0);
     Worley_noise w = Worley_noise();
     terrain2.addTerrainNode(w, NoisePunderationFunction::IDENTITY);
-    Noise result2 = terrain2.getTerrain(256, 256, 42);
+    Noise result2 = terrain2.getTerrain(1024, 1024, 42);
     result2.toPGM("./worley.pgm");
+}
 
-
+int main(void) {
+    // testWorley();
     // ---------------------- Test ---------------------------
-    int width = 10;
-    int height = 10;
-    int grid_size = 5;
+    Terrain terrain("test", time(NULL));
+    int width = 516;
+    int height = width;
     std::cout.precision(2);
-    Perlin perlin = Perlin(0, 255, grid_size);
-    Worley_noise worley = Worley_noise(0, 255);
+    int scale = 200;
+    Perlin globalShape = Perlin(0, 255, scale);
+    Perlin canyonShape = Perlin(0, 255, scale/2);
+    Perlin detailShape = Perlin(0, 255, scale/20);
+    Perlin microShape = Perlin(0, 255, scale/200); 
+    auto canyonPunderation = FilterPunderationFunction([](int value) { return value > 20; }, std::make_shared<ProductPunderationFunction<double>>(0.7));
+    auto detailPunderation = FilterPunderationFunction([](int value) { return value > 20; }, std::make_shared<ProductPunderationFunction<double>>(0.15));
+    auto microPunderation = FilterPunderationFunction([](int value) { return value > 20; }, std::make_shared<ProductPunderationFunction<double>>(0.05));
+    terrain.addTerrainNode(globalShape, NoisePunderationFunction::IDENTITY);
+    terrain.addTerrainNode(canyonShape, canyonPunderation);
+    terrain.addTerrainNode(detailShape, detailPunderation);
+    terrain.addTerrainNode(microShape, microPunderation);
 
-    auto identityFunction = IdentityPunderationFunction();
-    auto sumFunction = SumPunderationFunction(10);
-    terrain.addTerrainNode(perlin, identityFunction);
-    terrain.addTerrainNode(worley, sumFunction);
 
     Noise result = terrain.getTerrain(width, height, 2);
-    
-    BitMap<int> bitmap = result.getBitmap();
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++){
-            std::cout << bitmap.get(x, y) << "\t";
-        }
-
-        std::cout << std::endl;
-    }
+    std::cout << "Result: " << std::endl;
+    result.toPGM("./values.pgm");
+    result.toPPM("./heatmap.ppm", HEATMAP);
+    result.toPPM("./terrain.ppm", TERRAIN);
     return 0;
 }
+
+
